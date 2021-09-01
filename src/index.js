37eb59c0fd8cd44fac12c1194a3f6f062162b0bb
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-09-01 23:37:13 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './sass/main.scss';
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+const bodyEl = document.body
+const startBtn = document.querySelector('[data-action]')
+const stopBtn = document.querySelector('[data-action="stop"]')
+let timerId = null
+
+const colorSwitch = () => {
+    if(timerId){
+        return
+    }
+    timerId=setInterval(() => {
+        
+        const randomIntegerFromInterval = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        };
+    
+        bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0,5)]
+    },1000) 
+    
+}
+
+startBtn.addEventListener("click", colorSwitch)
+
+stopBtn.addEventListener('click', ()=>{clearInterval(timerId)})
+
+
+  
