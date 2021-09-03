5a544eded449cc9568941cf4048702b869ad2619
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-09-03 14:50:33 +0300

    Исправил

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import './sass/main.scss';
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
-const bodyEl = document.body
-const startBtn = document.querySelector('[data-action]')
-const stopBtn = document.querySelector('[data-action="stop"]')
-let timerId = null
+const bodyEl = document.body;
+const startBtn = document.querySelector('[data-action]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+let timerId = null;
 
 const colorSwitch = () => {
-    if(timerId){
-        return
-    }
-    timerId=setInterval(() => {
-        
-        const randomIntegerFromInterval = (min, max) => {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        };
-    
-        bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0,5)]
-    },1000) 
-    
-}
+  if (timerId) {
+    return;
+  }
+  timerId = setInterval(() => {
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
 
-startBtn.addEventListener("click", colorSwitch)
+    bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+};
 
-stopBtn.addEventListener('click', ()=>{clearInterval(timerId)})
+startBtn.addEventListener('click', colorSwitch);
 
-
-  
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  timerId = 0;
+});
